acacdbb9d6d96ad1581976fd6c3bbd45d7a19e2b
Angel Angelov <hextwoa@gmail.com>
2015-06-22 23:06:58 +0300

    rename manager to loader

--- src/stard/service.py
+++ src/stard/service.py
@@ -3,7 +3,7 @@ import xdg.BaseDirectory
 import os
 
 
-class Manager:
+class Loader:
     def __init__(self, config_dirs=[]):
         config_base_dirs = xdg.BaseDirectory.xdg_config_dirs + ['/etc']
         self.config_dirs = config_dirs + list(map(
@@ -27,7 +27,7 @@ class Manager:
         return (name, tuple(args), frozenset(kwargs.items()))
 
     def service(self, name, *args, **kwargs):
-        id = Manager.service_id(name, *args, **kwargs)
+        id = Loader.service_id(name, *args, **kwargs)
         if id not in self.services:
             service_module = SourceFileLoader(
                 name, self.find_file(name)
@@ -50,12 +50,12 @@ class BaseService:
     children = set()
     parents = set()
 
-    def __init__(self, manager, service_name, service_args, service_kwargs):
-        self.manager = manager
+    def __init__(self, loader, service_name, service_args, service_kwargs):
+        self.loader = loader
         self.service_name = service_name
         self.service_args = service_args
         self.service_kwargs = service_kwargs
-        self._hash = hash(Manager.service_id(
+        self._hash = hash(Loader.service_id(
             service_name, *service_args, **service_kwargs
         ))
         self.init_service(*service_args, **service_kwargs)
@@ -70,7 +70,7 @@ class BaseService:
         )
 
     def service(self, name, *args, **kwargs):
-        return self.manager.service(name, *args, **kwargs)
+        return self.loader.service(name, *args, **kwargs)
 
     def init_service(self, *args, **kwargs):
         pass
--- src/stard/service_test.py
+++ src/stard/service_test.py
@@ -2,29 +2,29 @@ import unittest
 import os
 import inspect
 
-from service import Manager
+from service import Loader
 from test_samples import empty
 
-class TestManager(unittest.TestCase):
+class TestLoader(unittest.TestCase):
     def assertSameClass(self, a, b):
         self.assertEqual(inspect.getsource(a), inspect.getsource(b))
 
     def setUp(self):
-        self.manager = Manager(config_dirs=['test_samples'])
+        self.loader = Loader(config_dirs=['test_samples'])
 
     def test_load_empty_service_from_given_config_dir(self):
-        sample = self.manager.service('empty')
+        sample = self.loader.service('empty')
         self.assertSameClass(sample.__class__, empty.Service)
 
     def test_load_service_twice_gives_same_object(self):
-        sample1 = self.manager.service('empty')
-        sample2 = self.manager.service('empty')
+        sample1 = self.loader.service('empty')
+        sample2 = self.loader.service('empty')
         self.assertIs(sample1, sample2)
 
     def test_populate_relatives(self):
-        father = self.manager.service('father')
-        child = self.manager.service('child')
-        self.manager.populate_relatives()
+        father = self.loader.service('father')
+        child = self.loader.service('child')
+        self.loader.populate_relatives()
         self.assertIn(father, child.parents)
         self.assertIn(child, father.children)
 
